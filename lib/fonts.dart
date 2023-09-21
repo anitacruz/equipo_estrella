1c55aaa3610b180a0ee3f9ed7bf08fa9e69c99ef
patrickmdey <patrickmdey@hotmail.com>
2023-09-21 15:25:43 -0300

    feat: fonts

---
+++ lib/fonts.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ManosFonts {
+  static const TextStyle h1 = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 24,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.18);
+
+  static const TextStyle h2 = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 0.15);
+
+  static const TextStyle sub1 = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.15);
+
+  static const TextStyle b1 = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.25);
+
+  static const TextStyle b2 = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.4);
+
+  static const TextStyle button = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 15,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 0.1);
+
+  static const TextStyle caption = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.4);
+
+  static const TextStyle overline = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 1.5);
+}
