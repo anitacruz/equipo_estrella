49f5d45e7fddca5cb51c109a5ebe62aa0b5f1e81
patrickmdey <patrickmdey@hotmail.com>
2023-09-21 16:03:32 -0300

    refactor: reorganized tokens into "commons"

--- lib/commons/fonts.dart
+++ lib/commons/fonts.dart
@@ -1,50 +1,58 @@
 import 'package:flutter/material.dart';
 
 class ManosFonts {
-  static const TextStyle h1 = TextStyle(
+  static TextStyle h1({Color color = Colors.black}) => TextStyle(
       fontFamily: 'Roboto',
+      color: color,
       fontSize: 24,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.18);
 
-  static const TextStyle h2 = TextStyle(
+  static TextStyle h2({Color color = Colors.black}) => TextStyle(
       fontFamily: 'Roboto',
+      color: color,
       fontSize: 20,
       fontWeight: FontWeight.w500,
       letterSpacing: 0.15);
 
-  static const TextStyle sub1 = TextStyle(
+  static TextStyle sub1({Color color = Colors.black}) => TextStyle(
       fontFamily: 'Roboto',
+      color: color,
       fontSize: 16,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.15);
 
-  static const TextStyle b1 = TextStyle(
+  static TextStyle b1({Color color = Colors.black}) => TextStyle(
       fontFamily: 'Roboto',
+      color: color,
       fontSize: 14,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.25);
 
-  static const TextStyle b2 = TextStyle(
+  static TextStyle b2({Color color = Colors.black}) => TextStyle(
       fontFamily: 'Roboto',
+      color: color,
       fontSize: 12,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.4);
 
-  static const TextStyle button = TextStyle(
+  static TextStyle button({Color color = Colors.black}) => TextStyle(
       fontFamily: 'Roboto',
+      color: color,
       fontSize: 15,
       fontWeight: FontWeight.w500,
       letterSpacing: 0.1);
 
-  static const TextStyle caption = TextStyle(
+  static TextStyle caption({Color color = Colors.black}) => TextStyle(
       fontFamily: 'Roboto',
+      color: color,
       fontSize: 12,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.4);
 
-  static const TextStyle overline = TextStyle(
+  static TextStyle overline({Color color = Colors.black}) => TextStyle(
       fontFamily: 'Roboto',
+      color: color,
       fontSize: 10,
       fontWeight: FontWeight.w400,
       letterSpacing: 1.5);
