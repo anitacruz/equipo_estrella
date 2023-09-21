f0f95446f610905ee80f9cba957d15f4451ba78a
patrickmdey <patrickmdey@hotmail.com>
2023-09-21 16:04:27 -0300

    feat: handmade chip widget

---
+++ lib/widgets/card_chip.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:equipo_estrella/commons/colors.dart';
+
+import '../commons/fonts.dart';
+
+class CardChip extends StatelessWidget {
+  const CardChip({Key? key, required this.amount, required this.isAvailable})
+      : super(key: key);
+
+  final int amount;
+  final bool isAvailable;
+  @override
+  Widget build(BuildContext context) {
+    // return Chip(
+    //     backgroundColor:
+    //         isAvailable ? ManosColors.secondary80 : ManosColors.neutral25,
+    //     // labelStyle: TextStyle(color: ManosColors.secondary200),
+    //     label: Row(
+    //       children: [
+    //         Text("Vacantes:",
+    //             style: ManosFonts.sub1(
+    //                 color: isAvailable
+    //                     ? ManosColors.secondary200
+    //                     : ManosColors.neutral0)),
+    //         const SizedBox(width: 15),
+    //         Icon(Icons.person,
+    //             color: isAvailable
+    //                 ? ManosColors.secondary200
+    //                 : ManosColors.secondary80,
+    //             size: 24),
+    //         Text('$amount',
+    //             style: ManosFonts.b1(
+    //                 color: isAvailable
+    //                     ? ManosColors.secondary200
+    //                     : ManosColors.secondary80)),
+    //       ],
+    //     ));
+
+    return Center(
+        child: Container(
+            height: 28,
+            alignment: Alignment.center,
+            padding:
+                const EdgeInsets.only(left: 8, right: 8, top: 4, bottom: 4),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color:
+                  //#CAE5FB
+                  isAvailable ? ManosColors.secondary25 : ManosColors.neutral25,
+            ),
+            // color:
+            //     isAvailable ? ManosColors.secondary80 : ManosColors.neutral25,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Vacantes:",
+                    style: ManosFonts.sub1(color: ManosColors.neutral0)),
+                const SizedBox(width: 8),
+                Icon(Icons.person,
+                    color: isAvailable
+                        ? ManosColors.secondary200
+                        : ManosColors.secondary80,
+                    size: 24),
+                Text('$amount',
+                    style: ManosFonts.b1(
+                        color: isAvailable
+                            ? ManosColors.secondary200
+                            : ManosColors.secondary80)),
+              ],
+            )));
+  }
+}
