6a28128c4968175e6b9ee373b9e3e2eaf6be5485
patrickmdey <patrickmdey@hotmail.com>
2023-11-17 08:08:44 -0300

    feat: activity card flow

--- lib/views/home_tab.dart
+++ lib/views/home_tab.dart
@@ -1,122 +1,74 @@
-// import 'package:equipo_estrella/controllers/user_controller.dart';
-// import 'package:equipo_estrella/controllers/volunteering_controller.dart';
-// import 'package:equipo_estrella/models/volunteering_model.dart';
-// import 'package:equipo_estrella/widgets/cards/activity_card.dart';
-// import 'package:equipo_estrella/widgets/scrollable_volunteering_card_list.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:logger/logger.dart';
-
-// var logger = Logger();
-
-// class HomeTab extends ConsumerStatefulWidget {
-//   const HomeTab({Key? key}) : super(key: key);
-
-//   @override
-//   ConsumerState<ConsumerStatefulWidget> createState() => _HomeTabState();
-// }
-
-// class _HomeTabState extends ConsumerState<HomeTab> {
-//   VolunteeringModel? _volunteeringModel;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     initializeState();
-//   }
-
-//   Future<void> initializeState() async {
-//     var volunteeringController =
-//         ref.read(volunteeringControllerProvider.notifier);
-
-//     var userController = ref.read(userControllerProvider.notifier);
-
-//     var currentUser = await userController.getCurrUser();
-
-//     volunteeringController
-//         .getVolunteering(currentUser.currVolunteering)
-//         .then((value) => () {
-//               if (mounted) {
-//                 logger.w("ACA!");
-//                 setState(() {
-//                   _volunteeringModel = value;
-//                 });
-//               }
-//             });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     logger.i("VolunteeringModel: $_volunteeringModel");
-//     return Container(
-//         margin: const EdgeInsets.only(top: 24, left: 16, right: 16),
-//         child: Column(children: [
-//           const TextField(
-//               decoration: InputDecoration(
-//             // border: OutlineInputBorder(),
-//             labelText: 'Buscar',
-//             prefixIcon: Icon(Icons.search, size: 24),
-//           )),
-//           if (_volunteeringModel != null)
-//             Column(children: [
-//               const Padding(
-//                   padding: EdgeInsets.only(top: 32, bottom: 24),
-//                   child: Align(
-//                       alignment: AlignmentDirectional.centerStart,
-//                       child: Text(
-//                         "Tu actividad",
-//                         style: TextStyle(fontSize: 24),
-//                       ))),
-//               ActivityCard(
-//                   category: _volunteeringModel!.category,
-//                   title: _volunteeringModel!.title,
-//                   id: _volunteeringModel!.id)
-//             ]),
-//           const Padding(
-//               padding: EdgeInsets.only(top: 32, bottom: 24),
-//               child: Align(
-//                   alignment: AlignmentDirectional.centerStart,
-//                   child: Text(
-//                     "Voluntariados",
-//                     style: TextStyle(fontSize: 24),
-//                   ))),
-//           const ScrollableVolunteeringCardList(),
-//         ]));
-//   }
-// }
-
+import 'package:equipo_estrella/controllers/user_controller.dart';
+import 'package:equipo_estrella/controllers/volunteering_controller.dart';
+import 'package:equipo_estrella/models/volunteering_model.dart';
 import 'package:equipo_estrella/widgets/cards/activity_card.dart';
 import 'package:equipo_estrella/widgets/scrollable_volunteering_card_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 
-class HomeTab extends StatelessWidget {
-  const HomeTab({super.key});
+var logger = Logger();
+
+class HomeTab extends ConsumerStatefulWidget {
+  const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends ConsumerState<HomeTab> {
+  VolunteeringModel? _volunteeringModel;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeState();
+  }
+
+  Future<void> initializeState() async {
+    var volunteeringController =
+        ref.read(volunteeringControllerProvider.notifier);
+
+    var userController = ref.read(userControllerProvider.notifier);
+
+    var currentUser = await userController.getCurrUser();
+
+    logger.i(
+        "Current user: ${currentUser.id} ${currentUser.email} ${currentUser.currVolunteering}");
+
+    var currVol = await volunteeringController
+        .getVolunteering(currentUser.currVolunteering);
+
+    setState(() {
+      _volunteeringModel = currVol;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    logger.i("VolunteeringModel: $_volunteeringModel");
     return Container(
         margin: const EdgeInsets.only(top: 24, left: 16, right: 16),
-        child: const Column(children: [
-          TextField(
+        child: Column(children: [
+          const TextField(
               decoration: InputDecoration(
             // border: OutlineInputBorder(),
             labelText: 'Buscar',
             prefixIcon: Icon(Icons.search, size: 24),
           )),
-          Padding(
-              padding: EdgeInsets.only(top: 32, bottom: 24),
-              child: Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text(
-                    "Tu actividad",
-                    style: TextStyle(fontSize: 24),
-                  ))),
-          ActivityCard(
-              category: "acción social",
-              title: "un techo para mi país",
-              id: "1"),
-          // Expanded(child: NoVolunteerCard())
-          Padding(
+          if (_volunteeringModel != null)
+            Column(children: [
+              const Padding(
+                  padding: EdgeInsets.only(top: 32, bottom: 24),
+                  child: Align(
+                      alignment: AlignmentDirectional.centerStart,
+                      child: Text(
+                        "Tu actividad",
+                        style: TextStyle(fontSize: 24),
+                      ))),
+              ActivityCard(vModel: _volunteeringModel!),
+            ]),
+          const Padding(
               padding: EdgeInsets.only(top: 32, bottom: 24),
               child: Align(
                   alignment: AlignmentDirectional.centerStart,
@@ -124,9 +76,7 @@ class HomeTab extends StatelessWidget {
                     "Voluntariados",
                     style: TextStyle(fontSize: 24),
                   ))),
-          ScrollableVolunteeringCardList(),
+          const ScrollableVolunteeringCardList(),
         ]));
   }
 }
-
-//widgetChild: VolunteerCard(category: "Acción social",title: "Un techo para mi país")
--- lib/widgets/cards/activity_card.dart
+++ lib/widgets/cards/activity_card.dart
@@ -1,39 +1,46 @@
 import 'package:equipo_estrella/commons/colors.dart';
 import 'package:equipo_estrella/commons/fonts.dart';
 import 'package:equipo_estrella/commons/shadows.dart';
+import 'package:equipo_estrella/models/volunteering_model.dart';
+import 'package:equipo_estrella/views/expanded_volunteering.dart';
 import 'package:flutter/material.dart';
 
 class ActivityCard extends StatelessWidget {
-  const ActivityCard(
-      {Key? key, required this.category, required this.title, required this.id})
-      : super(key: key);
+  const ActivityCard({Key? key, required this.vModel}) : super(key: key);
 
-  final String id;
-  final String category;
-  final String title;
-
+  final VolunteeringModel vModel;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: ManosShadows.shadow1.copyWith(
-            borderRadius: const BorderRadius.all(Radius.circular(6)),
-            border: Border.all(color: ManosColors.primary100, width: 2),
-            color: ManosColors.primary10),
-        height: 73,
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return InkWell(
+        onTap: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ExpandedVolunteer(
+                            vModel: vModel,
+                          )))
+            },
+        child: Container(
+            decoration: ManosShadows.shadow1.copyWith(
+                borderRadius: const BorderRadius.all(Radius.circular(6)),
+                border: Border.all(color: ManosColors.primary100, width: 2),
+                color: ManosColors.primary10),
+            height: 73,
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(category.toUpperCase(), style: ManosFonts.overline()),
-                Text(title, style: ManosFonts.sub1())
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(vModel.category.toUpperCase(),
+                        style: ManosFonts.overline()),
+                    Text(vModel.title, style: ManosFonts.sub1())
+                  ],
+                ),
+                const Icon(Icons.location_on,
+                    color: ManosColors.primary100, size: 24)
               ],
-            ),
-            const Icon(Icons.location_on,
-                color: ManosColors.primary100, size: 24)
-          ],
-        ));
+            )));
   }
 }
