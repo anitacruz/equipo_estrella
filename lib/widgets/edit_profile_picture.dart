1eb4999459ce3994cd7dd2ae7239cdc1574fbd96
Ana Cruz <acruz@itba.edu.ar>
2023-11-17 12:49:03 -0300

    Otro intento del edit profile picture, siento que estoy mas cerca

--- lib/widgets/edit_profile_picture.dart
+++ lib/widgets/edit_profile_picture.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:equipo_estrella/commons/fonts.dart';
 import 'package:equipo_estrella/widgets/buttons/short_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -27,40 +29,20 @@ class _EditProfilePictureState extends State<EditProfilePicture> {
   File? _selectedImage;
 
   Future<void> _pickImage() async {
-    // final imagePicker = ImagePicker();
-    // final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
-    // logger.i(
-    //     "the name of the file is ${pickedFile!.name} and the extension is ${pickedFile.mimeType}. additionally, the path is ${pickedFile.path}");
-
-    // setState(() {
-    //   if (pickedFile != null) {
-    //     _selectedImage = File(pickedFile.path);
-    //   }
-
     try {
       final imagePicker = ImagePicker();
       final pickedFile = await imagePicker.pickImage(
           source: ImageSource.gallery, maxWidth: 1500, maxHeight: 1500);
       logger.i(
           "the name of the file is ${pickedFile!.name} and the extension is ${pickedFile.mimeType}. additionally, the path is ${pickedFile.path}");
-      if (pickedFile != null) return;
 
       final imageTemp = File(pickedFile.path);
+      logger.i("ya tengo el image temp!");
       setState(() => _selectedImage = imageTemp);
+      logger.i("despues de setear el estado");
     } catch (e) {
       logger.e("Error al seleccionar la imagen: $e");
     }
-    //   setState(() {
-    //     if (pickedFile != null) {
-    //       _selectedImage = File(pickedFile.path);
-    //     }
-    //   });
-    // } catch (e) {
-    //   logger.e("Error al seleccionar la imagen: $e");
-    // }
-
-    // });
-
     // Subir la imagen a Firebase Storage
     await _uploadImageToFirebase();
   }
@@ -70,37 +52,46 @@ class _EditProfilePictureState extends State<EditProfilePicture> {
       return;
     }
 
-    final fileName = basename(_selectedImage!.path);
-    final destination = 'files/$fileName';
-    logger.i("Upload img from path: ${destination}");
-    try {
-      final ref = FirebaseStorage.instance.ref(destination).child('fileaneta/');
-      await ref.putFile(_selectedImage!);
-    } catch (e) {
-      logger.e("Error al subir la imagen a Firebase Storage: $e");
-    }
-
-    // // Create a storage reference from our app
-    // final storageRef = FirebaseStorage.instance.ref();
-    // // Create a reference to "mountains.jpg"
-    // final mountainsRef = storageRef.child("mountains.jpg");
-
+    // final fileName = basename(_selectedImage!.path);
+    // final destination = 'files/$fileName';
+    // logger.i("Upload img from path: ${destination}");
     // try {
-    //   await mountainsRef.putFile(_selectedImage!);
-    // } on FirebaseException catch (e) {
-    //   // ...
-    // }
-
-    // try {
-    //   final userId = FirebaseAuth.instance.currentUser?.uid;
-    //   if (userId != null) {
-    //     final storageRef =
-    //         FirebaseStorage.instance.ref().child('profile_pics/$userId.jpg');
-    //     await storageRef.putFile(_image!);
-    //   }
+    //   final ref = FirebaseStorage.instance.ref(destination).child('fileaneta/');
+    //   await ref.putFile(_selectedImage!);
     // } catch (e) {
-    //   print('Error al subir la imagen a Firebase Storage: $e');
+    //   logger.e("Error al subir la imagen a Firebase Storage: $e");
     // }
+
+    //   // Create a storage reference from our app
+    //   logger.i("creando el storage ref");
+    //   final storageRef = FirebaseStorage.instance.ref();
+    //   // Create a reference to "mountains.jpg"
+    //   logger.i("creando el mountains ref");
+    //   final mountainsRef = storageRef.child("mountains.jpg");
+
+    //   try {
+    //     logger.i("estamos adentro del try, por mandar el put file...");
+    //     await mountainsRef.putFile(_selectedImage!);
+    //     logger.i("despues del await)");
+    //   } on FirebaseException catch (e) {
+    //     // ...
+    //   }
+    // }
+
+    try {
+      logger.i("Upload img from path: ${_selectedImage!.path}");
+      final userId = FirebaseAuth.instance.currentUser?.uid;
+      logger.i("el userId es: $userId");
+      if (userId != null) {
+        final storageRef =
+            FirebaseStorage.instance.ref().child('profile_pics/$userId.jpg');
+        logger.i("el storageRef es: $storageRef");
+        logger.i("a punto de hacer el put file");
+        await storageRef.putFile(_selectedImage!);
+      }
+    } catch (e) {
+      print('Error al subir la imagen a Firebase Storage: $e');
+    }
   }
 
   @override
